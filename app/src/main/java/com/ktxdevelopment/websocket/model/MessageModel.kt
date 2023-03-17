471f39ec419e5ae88c5a241e7b4f1ec54ab27f50
Neon <fatehzaliyev@gmail.com>
2023-03-18 00:06:25 +0400

    Created data models

---
+++ app/src/main/java/com/ktxdevelopment/websocket/model/MessageModel.kt
@@ -0,0 +1,6 @@
+package com.ktxdevelopment.websocket.model
+
+data class MessageModel(
+    var result: List<ResultItem>,
+    var total: Int
+)
---
+++ app/src/main/java/com/ktxdevelopment/websocket/model/ResponseModel.kt
@@ -0,0 +1,5 @@
+package com.ktxdevelopment.websocket.model
+
+data class ResponseModel(
+    var message: MessageModel
+)
